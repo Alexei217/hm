306b99c6d385a9aba59eb82d95c4c1d7e5d7b009
Alexei217 <tyagunov217@gmail.com>
2023-03-24 20:56:51 +0300

    dz2 done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
-    import Counter from "./components/Counter/Counter.vue";
-</script>
+    import style from "./App.module.css"; 
+    import {ref} from "vue";
+    const input = ref("");
+    const cards = ref([
+        { model: "Mercedes"},
+        { model: "BMW"},
+        { model: "Honda"},
+        { model: "Toyota"},
+        { model: "Audi"},
+        { model: "Ford"},
+    ]);
+    </script>
 
 <template>
-    <h1>Hello</h1>
-    <Counter></Counter>
+    <input v-model="input" :class="style.input" />
+    <template v-for="{ model } in cards">
+        <div
+            :class="style.card"
+            v-if="model.toLowerCase().includes(input.toLowerCase())"
+        >
+            <h1>{{ model }}</h1>   
+         </div>
+    </template>
 </template>
-
-<style scoped>
-    h1 {
-        color: rebeccapurple;
-        text-align: center;
-    }
-</style> 
